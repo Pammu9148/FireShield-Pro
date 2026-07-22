516ebc03627cbce3866a2661dec7643f3d8428d0
PramodRajkumar <pramodrajakumar9966@gmail.com>
2026-07-23 01:34:07 +0530

    Fixed Firebase Firestore export

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfrGOdJ9HuPsVasX_oLLPwva8EVB5-T_E",
-  authDomain: "fireshield-pro-80fed.firebaseapp.com",
-  projectId: "fireshield-pro-80fed",
-  storageBucket: "fireshield-pro-80fed.firebasestorage.app",
-  messagingSenderId: "311192126693",
-  appId: "1:311192126693:web:42486d5e9fc5823986400c"
+  // your Firebase config details
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
